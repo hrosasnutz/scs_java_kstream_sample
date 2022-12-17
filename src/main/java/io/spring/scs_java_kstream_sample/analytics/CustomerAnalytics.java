a1d06f7db8f15e1d2768528a763d55d6b56e188f
hrosasnutz <hrosasnutz.1991@gmail.com>
2022-12-17 13:29:12 -0500

    feat: add more samples of kafka streams
    Signed-off-by: hrosasnutz <hrosasnutz.1991@gmail.com>

--- src/main/java/io/spring/scs_java_kstream_sample/analytics/CustomerAnalytics.java
+++ src/main/java/io/spring/scs_java_kstream_sample/analytics/CustomerAnalytics.java
@@ -10,19 +10,47 @@ import org.springframework.context.annotation.Configuration;
 import java.util.function.Function;
 
 import io.spring.scs_java_kstream_sample.dto.customer.Customer;
+import io.spring.scs_java_kstream_sample.dto.customer.Type;
 import lombok.extern.slf4j.Slf4j;
 
-@Configuration
 @Slf4j
+@Configuration
 public class CustomerAnalytics {
     
     @Bean
-    public Function<KStream<byte[], Customer>, KStream<byte[], Long>> countCustomerPerTypes() {
+    public Function<KStream<byte[], Customer>, KStream<byte[], Long>> countCustomersPerTypes() {
         return customers -> customers
                 .map((k, v) -> new KeyValue<>(v.getType().name().getBytes(), 1L))
                 .groupByKey(Grouped.with(Serdes.ByteArray(), Serdes.Long()))
                 .count()
                 .toStream()
                 .peek((k, v) -> log.info("Total {} customers for {} type.", v, new String(k)));
+    }
+    
+    @Bean
+    public Function<KStream<byte[], Customer>, KStream<String, Long>> countCustomersPerCountries() {
+        return customers -> customers
+                .map((k, v) -> new KeyValue<>(v.getCountry(), 1L))
+                .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                .count()
+                .toStream()
+                .peek((k, v) -> log.info("Total {} customers for {} country.", v, k));
+    }
+
+    @Bean
+    public Function<KStream<byte[], Customer>, KStream<byte[], Customer>> filterActiveCustomers() {
+        return customers -> customers
+                .filter((k, v) -> v.getActive())
+                .peek((k, v) -> log.info("Customer active: {}", v));        
+    }
+    
+    @Bean
+    public Function<KStream<byte[], Customer>, KStream<byte[], Customer>[]> branchPrivilegedCustomers() {
+        return customers -> customers
+                .split()
+                .branch((k, v) -> Type.isPrivileged(v.getType()))
+                .defaultBranch()
+                .values()
+                .toArray(new KStream[0]);
     }
 }
--- src/main/java/io/spring/scs_java_kstream_sample/dto/customer/Type.java
+++ src/main/java/io/spring/scs_java_kstream_sample/dto/customer/Type.java
@@ -3,7 +3,13 @@ package io.spring.scs_java_kstream_sample.dto.customer;
 import java.util.Random;
 
 public enum Type {
-    INTERNAL, EXTERNAL, LOYAL, POTENTIAL, INTERMEDIATE, FORMER, UNKNOWN;
+    INTERNAL,
+    EXTERNAL,
+    LOYAL,
+    POTENTIAL,
+    INTERMEDIATE,
+    FORMER,
+    UNKNOWN;
     
     public static Type random() {
         var random = new Random().nextInt(6);
@@ -15,6 +21,13 @@ public enum Type {
             case 4 -> INTERMEDIATE;
             case 5 -> FORMER;
             default -> UNKNOWN;
+        };
+    }
+    
+    public static boolean isPrivileged(Type type) {
+        return switch (type) {
+            case INTERNAL, LOYAL, FORMER -> true;
+            default -> false;
         };
     }
 }
